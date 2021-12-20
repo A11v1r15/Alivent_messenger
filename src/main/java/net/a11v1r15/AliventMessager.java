aece5bb006e63cd77e21a7746649697d13f87d53
Álvaro Carvalho <a11v1r15@Rocketmail.com>
2021-12-20 12:27:37 -0300

    Revert "Version"
    This reverts commit 6fb7745b860321f629fcbe7b69add4238f6e4cad.

--- src/main/java/net/a11v1r15/AliventMessager.java
+++ src/main/java/net/a11v1r15/AliventMessager.java
@@ -8,7 +8,7 @@ public class AliventMessager implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LogManager.getLogger("Alivent");
+	public static final Logger LOGGER = LogManager.getLogger("alivent");
 
 	@Override
 	public void onInitialize() {
