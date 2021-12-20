fb13e9e0b8bfd9feabe5b937bf9eedb10446544e
Álvaro Carvalho <a11v1r15@Rocketmail.com>
2021-12-20 12:07:50 -0300

    It Works!

--- src/main/java/net/a11v1r15/mixin/LivingEntityMixin.java
+++ src/main/java/net/a11v1r15/mixin/LivingEntityMixin.java
@@ -1,19 +1,25 @@
 package net.a11v1r15.mixin;
 
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.Util;
+import net.minecraft.world.GameRules;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 @Mixin(LivingEntity.class)
 public class LivingEntityMixin {
-	private static final Logger LOGGER2 = LogManager.getLogger("TheGrimReaper");
-
-	@Inject(at = @At(value = "TAIL"), method = "onDeath()V")
+	LivingEntity This = ((LivingEntity)(Object)this);
+	@Inject(at = @At(value = "HEAD"), method = "onDeath()V")
 	private void init(CallbackInfo info) {
-		LOGGER2.info(((LivingEntity)(Object)this).getDamageTracker().getDeathMessage().getString());
+        if (!This.world.isClient &&
+		     This.hasCustomName() &&
+		     This.world.getGameRules().getBoolean(GameRules.SHOW_DEATH_MESSAGES) &&
+		     This.world instanceof ServerWorld) {
+				This.world.getPlayers().forEach(player -> player.sendSystemMessage(This.getDamageTracker().getDeathMessage(), Util.NIL_UUID));
+		}
 	}
 }
