8ee021411f4fcc8d07e89f784633fdffe8250315
Álvaro Carvalho <a11v1r15@Rocketmail.com>
2021-12-19 21:37:04 -0300

    More configurations

--- src/main/java/net/a11v1r15/AliventMessager.java
+++ src/main/java/net/a11v1r15/AliventMessager.java
@@ -1,10 +1,10 @@
-package net.fabricmc.example;
+package net.a11v1r15;
 
 import net.fabricmc.api.ModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class ExampleMod implements ModInitializer {
+public class AliventMessager implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
