f71c7174e11660c5ddc943303376eacd739522cf
Álvaro Carvalho <a11v1r15@Rocketmail.com>
2021-12-28 23:58:15 -0300

    Put Villager code into LivingEntity, added ZombieVillager while converting.

--- src/main/java/net/a11v1r15/alivent/messager/mixin/LivingEntityMixin.java
+++ src/main/java/net/a11v1r15/alivent/messager/mixin/LivingEntityMixin.java
@@ -1,7 +1,9 @@
 package net.a11v1r15.alivent.messager.mixin;
 
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.mob.ZombieVillagerEntity;
 import net.minecraft.entity.passive.TameableEntity;
+import net.minecraft.entity.passive.VillagerEntity;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.Util;
 import net.minecraft.world.GameRules;
@@ -13,14 +15,19 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LivingEntity.class)
 public class LivingEntityMixin {
-	LivingEntity This = ((LivingEntity)(Object)this);
-	@Inject(at = @At(value = "HEAD"), method = "onDeath(Lnet/minecraft/entity/damage/DamageSource;)V")
-	private void init(CallbackInfo info) {
+    LivingEntity This = ((LivingEntity)(Object) this);
+    @Inject(at = @At(value = "HEAD"), method = "onDeath(Lnet/minecraft/entity/damage/DamageSource;)V")
+    private void init(CallbackInfo info) {
         if (!This.world.isClient &&
-		     This.world.getGameRules().getBoolean(GameRules.SHOW_DEATH_MESSAGES) &&
-		     This.hasCustomName()) {
-				 final boolean test = (This instanceof TameableEntity && ((TameableEntity)This).getOwner() instanceof ServerPlayerEntity);
-				 This.world.getPlayers().forEach(player -> {if(!(test && ((TameableEntity)This).getOwnerUuid() == player.getUuid())) player.sendSystemMessage(This.getDamageTracker().getDeathMessage(), Util.NIL_UUID);});
-		}
-	}
+            This.world.getGameRules().getBoolean(GameRules.SHOW_DEATH_MESSAGES)) {
+            if (This.hasCustomName()) {
+                final boolean test = (This instanceof TameableEntity && ((TameableEntity) This).getOwner() instanceof ServerPlayerEntity);
+                This.world.getPlayers().forEach(player -> {if (!(test && ((TameableEntity) This).getOwnerUuid() == player.getUuid())) player.sendSystemMessage(This.getDamageTracker().getDeathMessage(), Util.NIL_UUID);});
+            } else if (This instanceof VillagerEntity) {
+                This.world.getPlayers().forEach(player -> player.sendSystemMessage(This.getDamageTracker().getDeathMessage(), Util.NIL_UUID));
+            } else if (This instanceof ZombieVillagerEntity && (((ZombieVillagerEntity)This).isConverting() || false)) {
+                This.world.getPlayers().forEach(player -> player.sendSystemMessage(This.getDamageTracker().getDeathMessage(), Util.NIL_UUID));
+            }
+        }
+    }
 }
