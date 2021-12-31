36b0d64541ff1215d04f672f5d10ac1e2eda5bf2
Álvaro Carvalho <a11v1r15@Rocketmail.com>
2021-12-30 22:35:17 -0300

    Zombie Villagers have now a custom default name with their profession

---
+++ src/main/java/net/a11v1r15/alivent/messager/mixin/ZombieVillagerEntityMixin.java
@@ -0,0 +1,23 @@
+package net.a11v1r15.alivent.messager.mixin;
+
+import net.minecraft.entity.mob.ZombieVillagerEntity;
+import net.minecraft.text.Text;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.village.VillagerProfession;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(ZombieVillagerEntity.class)
+public class ZombieVillagerEntityMixin {
+	ZombieVillagerEntity This = ((ZombieVillagerEntity)(Object)this);
+    protected Text getDefaultName() {
+		if(!This.getVillagerData().getProfession().equals(VillagerProfession.NONE)){
+			TranslatableText profession = new TranslatableText(This.getType().getTranslationKey().replace("zombie_", "") + "." + Registry.VILLAGER_PROFESSION.getId(This.getVillagerData().getProfession()).getPath());
+        	TranslatableText zombie = new TranslatableText("entity.minecraft.zombie_villager");
+			return new LiteralText(profession.getString() + " (" + zombie.getString() + ")");
+		}
+		return new TranslatableText("entity.minecraft.zombie_villager");
+    }
+}
