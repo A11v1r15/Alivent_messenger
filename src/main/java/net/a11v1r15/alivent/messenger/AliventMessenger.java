6cdfe3fc14faa52f917a3efee6fa92653310675a
Álvaro Carvalho <a11v1r15@Rocketmail.com>
2022-01-06 23:16:44 -0300

    Troll whitespace?

--- src/main/java/net/a11v1r15/alivent/messenger/AliventMessenger.java
+++ src/main/java/net/a11v1r15/alivent/messenger/AliventMessenger.java
@@ -9,20 +9,20 @@ import net.minecraft.world.GameRules;
 import net.minecraft.world.GameRules.BooleanRule;
 
 public class AliventMessenger implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LogManager.getLogger("Alivent");
+    // This logger is used to write text to the console and the log file.
+    // It is considered best practice to use your mod id as the logger's name.
+    // That way, it's clear which mod wrote info, warnings, and errors.
+    public static final Logger LOGGER = LogManager.getLogger("Alivent");
 
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-		public static final GameRules.Key<GameRules.BooleanRule> ALIVENT_ALL_MOBS = GameRuleRegistry.register("aliventAllMobs", Category.MOBS, GameRuleFactory.createBooleanRule(false));
-		public static final GameRules.Key<GameRules.BooleanRule> ALIVENT_VILLAGERS = GameRuleRegistry.register("aliventVillagers", Category.MOBS, GameRuleFactory.createBooleanRule(true));
-		public static final GameRules.Key<GameRules.BooleanRule> ALIVENT_LORE_DROPS = GameRuleRegistry.register("aliventLoreDrops", Category.MOBS, GameRuleFactory.createBooleanRule(false));
+    @Override
+    public void onInitialize() {
+        // This code runs as soon as Minecraft is in a mod-load-ready state.
+        // However, some things (like resources) may still be uninitialized.
+        // Proceed with mild caution.
+        public static final GameRules.Key<GameRules.BooleanRule> ALIVENT_ALL_MOBS = GameRuleRegistry.register("aliventAllMobs", Category.MOBS, GameRuleFactory.createBooleanRule(false));
+        public static final GameRules.Key<GameRules.BooleanRule> ALIVENT_VILLAGERS = GameRuleRegistry.register("aliventVillagers", Category.MOBS, GameRuleFactory.createBooleanRule(true));
+        public static final GameRules.Key<GameRules.BooleanRule> ALIVENT_LORE_DROPS = GameRuleRegistry.register("aliventLoreDrops", Category.MOBS, GameRuleFactory.createBooleanRule(false));
 
-		LOGGER.info("TheGrimReaper entered the chat");
-	}
+        LOGGER.info("TheGrimReaper entered the chat");
+    }
 }
