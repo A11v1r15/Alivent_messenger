d482806b27f423cff0c9b3df276504217986dac7
Álvaro Carvalho <a11v1r15@Rocketmail.com>
2022-01-08 17:40:51 -0300

    Update AliventMessenger.java

--- src/main/java/net/a11v1r15/alivent/messenger/AliventMessenger.java
+++ src/main/java/net/a11v1r15/alivent/messenger/AliventMessenger.java
@@ -6,7 +6,7 @@ import org.apache.logging.log4j.Logger;
 import net.fabricmc.fabric.api.gamerule.v1.GameRuleFactory;
 import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
 import net.minecraft.world.GameRules;
-import net.minecraft.world.GameRules.BooleanRule;
+import net.minecraft.world.GameRules.Category;
 
 public class AliventMessenger implements ModInitializer {
     // This logger is used to write text to the console and the log file.
@@ -16,7 +16,7 @@ public class AliventMessenger implements ModInitializer {
 
     public static final GameRules.Key<GameRules.BooleanRule> ALIVENT_ALL_MOBS = GameRuleRegistry.register("aliventAllMobs", Category.MOBS, GameRuleFactory.createBooleanRule(false));
     public static final GameRules.Key<GameRules.BooleanRule> ALIVENT_VILLAGERS = GameRuleRegistry.register("aliventVillagers", Category.MOBS, GameRuleFactory.createBooleanRule(true));
-    public static final GameRules.Key<GameRules.BooleanRule> ALIVENT_LORE_DROPS = GameRuleRegistry.register("aliventLoreDrops", Category.MOBS, GameRuleFactory.createBooleanRule(false));
+    public static final GameRules.Key<GameRules.BooleanRule> ALIVENT_LORE_DROPS = GameRuleRegistry.register("aliventLoreDrops", Category.MOBS, GameRuleFactory.createBooleanRule(true));
 
     @Override
     public void onInitialize() {
