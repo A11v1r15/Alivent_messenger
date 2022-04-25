e3ec8b32a9c3f4b64f58b444c19ba1376d1d6acc
Álvaro Carvalho <Alvaro_Carvalho@alu.ufc.br>
2022-04-25 12:51:26 -0300

    New Logger

--- src/main/java/net/a11v1r15/alivent/messenger/mixin/LivingEntityMixin.java
+++ src/main/java/net/a11v1r15/alivent/messenger/mixin/LivingEntityMixin.java
@@ -13,7 +13,7 @@ import net.minecraft.util.Util;
 import net.minecraft.world.GameRules;
 import net.minecraft.world.World;
 
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
 
 import com.llamalad7.mixinextras.injector.WrapWithCondition;
 
@@ -51,7 +51,7 @@ extends Entity {
 
     @WrapWithCondition(
 	    method = "onDeath(Lnet/minecraft/entity/damage/DamageSource;)V", 
-	    at = @At(value = "INVOKE", target = "Lorg/apache/logging/log4j/Logger;info(Ljava/lang/String;Ljava/lang/Object;Ljava/lang/Object;)V")
+	    at = @At(value = "INVOKE", target = "Lorg/slf4j/Logger;info(Ljava/lang/String;Ljava/lang/Object;Ljava/lang/Object;)V")
     )
     private boolean jnit(Logger instance, String message, Object p0, Object p1) {
         return !this.world.getGameRules().getBoolean(AliventRules.ALIVENT_SERVER_SPAM_REMOVER);
