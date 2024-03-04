e5fe49f806cbe243bdbaa8b6896af7eec2aa4886
Álvaro Carvalho <a11v1r15@Rocketmail.com>
2024-03-04 01:35:29 -0300

    :bug: Add preLaunch again

---
+++ src/main/java/net/a11v1r15/aliventmessenger/preLaunch.java
@@ -0,0 +1,14 @@
+package net.a11v1r15.aliventmessenger;
+
+import com.llamalad7.mixinextras.MixinExtrasBootstrap;
+
+import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint;
+
+public class preLaunch implements PreLaunchEntrypoint {
+
+    @Override
+    public void onPreLaunch() {
+        MixinExtrasBootstrap.init();
+    }
+    
+}
