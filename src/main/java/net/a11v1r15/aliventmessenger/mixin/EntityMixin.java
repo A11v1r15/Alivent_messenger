1678ff1cd740a645b40c7feef7ac4b4f50dcd788
Álvaro Carvalho <a11v1r15@Rocketmail.com>
2024-03-26 21:38:38 -0300

    Revert "AliventMessengerMixinPlugin"
    This reverts commits 5890afec33c1b068bc62c151795757c9e8aa1c2d and 4b90fa4ed8c3dbe79d297e516923667d67ea3478

--- src/main/java/net/a11v1r15/aliventmessenger/mixin/EntityMixin.java
+++ src/main/java/net/a11v1r15/aliventmessenger/mixin/EntityMixin.java
@@ -8,37 +8,36 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 
 import net.a11v1r15.aliventmessenger.AliventMessengerConfig;
+import net.minecraft.SharedConstants;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ItemEntity;
-import net.minecraft.item.ItemStack;
 import net.minecraft.item.SpawnEggItem;
-import net.minecraft.nbt.NbtCompound;
-import net.minecraft.nbt.NbtList;
-import net.minecraft.nbt.NbtString;
 import net.minecraft.server.command.CommandOutput;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import net.minecraft.util.Nameable;
 import net.minecraft.world.World;
 import net.minecraft.world.entity.EntityLike;
+import net.minecraft.component.type.LoreComponent;
+import net.minecraft.component.DataComponentTypes;
 
 @Mixin(Entity.class)
-public abstract class EntityMixinOneTwentyZero
+public abstract class EntityMixin
 implements Nameable,
 EntityLike,
 CommandOutput {
     @Shadow public World world;
+    @SuppressWarnings("deprecation")
     @ModifyVariable(
         method = "dropStack(Lnet/minecraft/item/ItemStack;F)Lnet/minecraft/entity/ItemEntity;",
         at = @At(value = "STORE"), ordinal = 0
         )
     private ItemEntity aliventMessenger$giveLoreNameToDroppings(ItemEntity x) {
         if(AliventMessengerConfig.loreDrops && this.hasCustomName()){
-            NbtList lore = new NbtList();
-            lore.add(0, NbtString.of("{\"text\":\"" + this.getCustomName().getString() + "\"}"));
-            NbtCompound display = x.getStack().getOrCreateSubNbt(ItemStack.DISPLAY_KEY);
-            display.put(ItemStack.LORE_KEY, lore);
+            LoreComponent lore = LoreComponent.DEFAULT;
+            lore.with(Text.literal(this.getCustomName().getString()));
+            x.getStack().set(DataComponentTypes.LORE, lore);
         }
         return x;
     }
